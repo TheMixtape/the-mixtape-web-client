cbcdebf8310e45cd5d6954604d74d58c2832fdd2
Patrick Shim <nasuk0610@gmail.com>
2017-08-30 08:39:57 +0900

    init webpack and simple react app

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './app/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          { loader: "babel-loader" }
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx$/,
+        use: [
+          { loader: "babel-loader" }
+        ],
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
+};
